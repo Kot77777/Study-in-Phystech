297adf3b8c6f68e45e974892bcbbe6863d130d61
Танов Константин <tanov.konstanti@yandex.ru>
2023-11-08 17:08:58 +0300

    wave

--- Авт-измер/obrabotka.py
+++ Авт-измер/obrabotka.py
@@ -8,8 +8,7 @@ num = np.arange(1, 192)
 time_array = num*(1/23.731946900172023)
 napr_array = data_array*0.0129
 
-plt.plot(time_array, napr_array, marker = "*", label = "V(t)", markevery=10, color = "red", mec = "blue",
-         fillstyle = "full", markersize = "5", mfc = "blue")
+plt.plot(time_array, napr_array, alpha = 0.5, marker = "*", label = "V(t)", markevery=10, color = "red")
 plt.minorticks_on()
 plt.xlabel(r'$x$', fontsize=16)
 
@@ -19,14 +18,8 @@ plt.ylim([0, 3])
 plt.grid(which='major')
 plt.grid(which='minor', linestyle=':')
 plt.tight_layout()
-plt.xlabel('Время, с', fontsize=10)
-plt.ylabel('Напряжение, В', fontsize=10)
-plt.title("Поцесс заряда и разряда конденсатора в RC-цепочке")
-plt.text(6, 1.2, 'Время заряда = 7.5c', style='italic',
-         bbox={'facecolor': 'b', 'alpha': 0.1, 'pad': 5})
-plt.text(6, 1, 'Время разряда = 0.55', style='italic',
-         bbox={'facecolor': 'b', 'alpha': 0.1, 'pad': 5})
+plt.xlabel('Время, с', fontsize=10, fontweight='bold')
+plt.ylabel('Напряжение, В', fontsize=10, fontweight='bold')
 
-plt.savefig("график.png")
 plt.legend()
 plt.show()
---
+++ Ларькин/waveFunctions.py
@@ -0,0 +1,5 @@
+# Bad news:
+# - There are no functions in this file =(
+
+# Good news:
+# - You already know everything for this lab =)
